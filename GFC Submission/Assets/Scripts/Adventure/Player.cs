cf797e60ffaa02b5c6bd577ae93570f2df5e0cc6
WenxinLi0317 <wenxinl8@nycstudents.net>
2025-01-27 13:18:42 -0500

    sprite flip animation

--- GFC Submission/Assets/Scripts/Adventure/Player.cs
+++ GFC Submission/Assets/Scripts/Adventure/Player.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
-
     private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 moveInput;
 
     private void Start()
     {
+
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector2 velocity = new Vector2(horizontalInput, verticalInput) * speed;
-        rb.velocity = velocity;
+
+        moveInput = new Vector2(horizontalInput, verticalInput);
+
+        //Vector2 velocity = new Vector2(horizontalInput, verticalInput) * speed;
+        rb.velocity = moveInput * speed;
+
+        if (moveInput == Vector2.zero)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            animator.SetBool("isWalking", true);
+
+            if (moveInput.x > 0)
+
+            {
+                spriteRenderer.flipX = true;
+            }
+            else if (moveInput.x < 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
     }
 }
