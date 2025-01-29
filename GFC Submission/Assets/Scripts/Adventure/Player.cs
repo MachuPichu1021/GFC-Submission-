dfe09e51b723ebccb50a3e0361ca59a4fee9ad05
Machu-Pichu1021 <machupichu1021@gmail.com>
2025-01-29 15:11:00 -0500

    Finished animations
    Also normalized the movement vector so you don't go faster when moving diagonally.

--- GFC Submission/Assets/Scripts/Adventure/Player.cs
+++ GFC Submission/Assets/Scripts/Adventure/Player.cs
@@ -7,44 +7,21 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D rb;
     private Animator animator;
-    private SpriteRenderer spriteRenderer;
-    private Vector2 moveInput;
 
     private void Start()
     {
-
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        moveInput = new Vector2(horizontalInput, verticalInput);
-
-        //Vector2 velocity = new Vector2(horizontalInput, verticalInput) * speed;
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector2 moveInput = new Vector2(horizontalInput, verticalInput).normalized;
         rb.velocity = moveInput * speed;
 
-        if (moveInput == Vector2.zero)
-        {
-            animator.SetBool("isWalking", false);
-        }
-        else
-        {
-            animator.SetBool("isWalking", true);
-
-            if (moveInput.x > 0)
-
-            {
-                spriteRenderer.flipX = true;
-            }
-            else if (moveInput.x < 0)
-            {
-                spriteRenderer.flipX = false;
-            }
-        }
+        animator.SetFloat("Vertical Input", verticalInput);
+        animator.SetFloat("Horizontal Input", horizontalInput);
     }
 }
