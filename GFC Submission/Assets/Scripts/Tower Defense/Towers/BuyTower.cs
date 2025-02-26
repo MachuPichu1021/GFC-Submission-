9c2eb354e1d4343cae6ee640c5e3a847cf065ce4
KoeruLin <167535514+KoeruLin@users.noreply.github.com>
2025-02-25 21:25:46 -0500

    Refactored buy script for button

--- GFC Submission/Assets/Scripts/Tower Defense/Towers/BuyTower.cs
+++ GFC Submission/Assets/Scripts/Tower Defense/Towers/BuyTower.cs
@@ -4,69 +4,37 @@ using UnityEngine;
 
 public class BuyTower : MonoBehaviour 
 {
-    [SerializeField] private GameObject[] towers;
     [SerializeField] private GameObject selectedTower;
     [SerializeField] private GameObject selectedTowerPreview;
     private Vector2 mousePos;
-    
-    private void Start()
-    {   
+    private bool onClick = false;
 
-    }
-
-    private void Update()
+    public void Update()
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
 
-        Debug.Log(hit.collider?.tag);
+        Debug.Log(mousePos.x + " " + mousePos.y);
         
-        if (hit.collider != null && Input.GetMouseButtonDown(0) && hit.collider.gameObject.layer == 5)
-        {
-            string tag = hit.collider.tag;
-
-            for (int i = 0; i < towers.Length; i++)
-            {
-                if (towers[i].CompareTag(tag))
-                {
-                    selectedTower = towers[i];
-                    break;
-                }
-            }
-        }
-
-        if (selectedTower != null)
-        {
-            PreviewTower();
-            
-            if (hit.collider == null)
-            {
-                PlaceTower();
-            }
-        }
-    }
-
-    private void PreviewTower()
-    {  
-        if (selectedTower != null)
-        {
-            selectedTowerPreview.GetComponent<SpriteRenderer>().sprite = selectedTower.GetComponent<SpriteRenderer>().sprite;
-        }
-        else
-        {
-            selectedTowerPreview.GetComponent<SpriteRenderer>().sprite = null;
-        }
-
         selectedTowerPreview.transform.position = mousePos;
+        
+        if (onClick)
+        {
+           if (MoneyManager.instance.Money >= selectedTower.GetComponent<Tower>().PlacementCost && Input.GetMouseButtonDown(0))
+            {
+                Instantiate(selectedTower, mousePos, Quaternion.identity);
+                MoneyManager.instance.ChangeMoney(-selectedTower.GetComponent<Tower>().PlacementCost);
+                selectedTowerPreview.SetActive(false);
+                onClick = false;
+            } 
+        }
+
+
     }
 
-    private void PlaceTower()
+    public void PreviewTower()
     {
-        if (MoneyManager.instance.Money >= selectedTower.GetComponent<Tower>().PlacementCost && Input.GetMouseButtonDown(0))
-        {
-            Instantiate(selectedTower, mousePos, Quaternion.identity);
-            MoneyManager.instance.ChangeMoney(-selectedTower.GetComponent<Tower>().PlacementCost);
-            selectedTower = null;
-        }
+        selectedTowerPreview.GetComponent<SpriteRenderer>().sprite = selectedTower.GetComponent<SpriteRenderer>().sprite;
+        onClick = true;
+        selectedTowerPreview.SetActive(true);
     }
 }
